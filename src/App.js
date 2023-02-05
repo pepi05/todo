1e7282ffb2395d8e89e33b3d94ab954dbe981030
Petar <72278293+pepi05@users.noreply.github.com>
2023-02-05 22:23:26 +0100

    implemented add todo, delete todo item and complete todo item functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,94 @@
+import { useState } from "react";
+import { AiOutlineDelete, AiOutlineCheck, AiOutlineEdit } from "react-icons/ai";
+import "./app.css";
+
 const App = () => {
-  return <div className="App">Petar ToDo</div>;
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.floor(Math.random() * 1000),
+      name: todo,
+      date: new Date().toLocaleString(),
+      active: true,
+    };
+
+    if (todo.length <= 2) return;
+    setTodos((prevState) => [...prevState, newTodo]);
+    setInputValue("");
+  };
+
+  const deleteTodo = (todoId) => {
+    const filteredTodo = todos.filter((todo) => todo.id !== todoId);
+    setTodos(filteredTodo);
+  };
+
+  const completeTodo = (todoId) => {
+    const completedTodo = todos.map((todo) => {
+      if (todo.id === todoId) {
+        return {
+          ...todo,
+          active: !todo.active,
+        };
+      }
+      return todo;
+    });
+    setTodos(completedTodo);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="appTitle">Petar Todo</h1>
+      <div className="formContainer">
+        <input
+          className="formInput"
+          type="text"
+          id="text"
+          onChange={onInputChange}
+          value={inputValue}
+          placeholder="Enter Todo"
+        />
+        <button className="addButton" onClick={() => addTodo(inputValue)}>
+          Add
+        </button>
+      </div>
+
+      {todos.map((todo) => {
+        return (
+          <div
+            key={todo.id}
+            className={`${
+              !todo.active && "todoCompletedContainer"
+            } todoContainer`}
+          >
+            <h2 className={`${!todo.active && "completedTodo"} todoName`}>
+              {todo.name}
+            </h2>
+            <div className="todoIconsContainer">
+              <AiOutlineEdit className="icon" size={20} />
+              <AiOutlineCheck
+                onClick={() => completeTodo(todo.id)}
+                className="icon"
+                size={20}
+                color="green"
+              />
+              <AiOutlineDelete
+                onClick={() => deleteTodo(todo.id)}
+                className="icon"
+                size={20}
+                color="red"
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
